f7358b22c281d7fc5597a32b503678b3ce0fce5e
Nathan Esterkyn <nesterkyn@mines.edu>
2024-02-16 20:27:47 -0700

    Testing

--- c_lib/Lab2_Tasks.c
+++ c_lib/Lab2_Tasks.c
@@ -1,25 +1,44 @@
 #include "Lab2_Tasks.h"
 
 void Send_Time_Now( float _time_since_last){
+    static float temp_run_period = 0;
+    temp_run_period = task_time_loop.run_period;
+    char time_cmd;
+    if(temp_run_period<0){
+        time_cmd = 't';
+    }
+    else{
+        time_cmd = 'T';
+    }
+
     float time = Timing_Get_Time_Sec();
     timing_data.cmd = '0';
     timing_data.time = time;
-    USB_Send_Msg("ccf", 't' , &timing_data , sizeof(timing_data));
+    USB_Send_Msg("ccf", time_cmd , &timing_data , sizeof(timing_data));
 }
 
 void Send_Loop_Time( float _time_since_last){
 
     static bool loop_completed_flag = false; // an undeclared flag to tell if the loop has been completed
+    static float temp_run_period = 0;
 
     if(!loop_completed_flag) { // if the loop hasn't been completed
         loop_completed_flag = true; // indicate the loop has been
+        temp_run_period = task_time_loop.run_period;
         task_time_loop.run_period = 0; // has the loop run indefinitely
     }
     else {
+        char time_cmd;
+        if(temp_run_period<0){
+            time_cmd = 't';
+        }
+        else{
+            time_cmd = 'T';
+        }
         timing_data.cmd = '1';
         timing_data.time = _time_since_last;
-        USB_Send_Msg("ccf", 't' , &timing_data , sizeof(timing_data));
+        USB_Send_Msg("ccf", time_cmd , &timing_data , sizeof(timing_data));
         loop_completed_flag = false; // indicate the loop hasn't been completed
-        task_time_loop.run_period = -1; // has the loop run once
+        task_time_loop.run_period = temp_run_period; // has the loop run once
     }
 }
--- c_lib/Lab2_Tasks.h
+++ c_lib/Lab2_Tasks.h
@@ -56,5 +56,7 @@ Task_t task_message_handling_watchdog;  ///<-- LAB 2: Watchdog Timer for Flusing
 // message handeling and the Lab main loops.
 void Send_Loop_Time( float _time_since_last );
 void Send_Time_Now( float _time_since_last );
+void Send_Loop_Time_T( float _time_since_last );
+void Send_Time_Now_T( float _time_since_last );
 
 #endif  // ifndef LAB2_TASKS_H
