2e9a69c0f847f9024310b33d56fca3905dfbe228
Nathan Esterkyn <nesterkyn@mines.edu>
2024-03-03 20:53:21 -0700

    Testing

--- c_lib/Lab3_Tasks.c
+++ c_lib/Lab3_Tasks.c
@@ -37,10 +37,13 @@ void Send_Battery_Voltage(float unused){
 void Send_Battery_Warning(float unused){
     float vbat = Filter_Last_Output(&voltage_filter);
     if (vbat <= LOW_BATTERY_THRESHOLD){
+        /*
         struct __attribute__((__packed__)) {char let[7]; float volt;} msg = {
             .let = {'B','A','T',' ','L','O','W'},
             .volt = vbat };
         // Send Warning to Serial that batteries need to be charged
         USB_Send_Msg("c7sf",'!', &msg, sizeof(msg));
+        */
+        USB_Send_Msg("!",'H', &vbat, sizeof(vbat));
     }
 }
