d05070322c40d33ac8f27beff64992ba2d694e81
Nathan Esterkyn <nesterkyn@mines.edu>
2024-03-03 20:43:07 -0700

    Testing

--- Lab3-Encoder/Lab3-Encoder.c
+++ Lab3-Encoder/Lab3-Encoder.c
@@ -91,7 +91,7 @@ void Initialize_Modules(float unused)
     Task_Activate(&task_message_handling,0);
 
     // Activate battery warning task to run every second
-    Task_Activate(&task_send_battery_warning,1);
+    Task_Activate(&task_send_battery_warning,1000);
 
 }
 
@@ -116,7 +116,7 @@ int main( void ){
         Task_Run_If_Ready(&task_send_battery_warning); // build this in
 
         if (!task_message_handling_watchdog.is_active){ // if the message handling watchdog isn't active (message timeout functionality)
-           Task_Activate(&task_message_handling_watchdog,0.25); // activate message handling watchdog to run every 0.25 seconds (250 ms)
+           Task_Activate(&task_message_handling_watchdog,250); // activate message handling watchdog to run every 0.25 seconds (250 ms)
         }
         Task_Run_If_Ready(&task_message_handling_watchdog); // run watchdog if ready
     }
