7dce49f3f99d483ad043c1a9347b2681a5b97042
Nathan Esterkyn <nesterkyn@mines.edu>
2024-03-11 17:40:37 -0600

    Testing

---
+++ c_lib/Lab4_Tasks.c
@@ -0,0 +1,45 @@
+#include "Lab3_Tasks.h"
+#include "Filter.h"
+
+void Send_Encoder_Value(float unused){
+    char command;
+    float runPeriod = task_send_encoder_value.run_period;
+    if(runPeriod<0){ // detects whether the command was 'e' or 'E'
+        command = 'e';
+    }else{
+        command = 'E';
+    }
+    struct{ // structure to store encoder values
+        float left;
+        float right;
+    }data;
+
+    data.left = Encoder_Counts_Left(); // write to structure using functions
+    data.right = Encoder_Counts_Right();
+    USB_Send_Msg("cff", command, &data, sizeof(data)); // send USB message
+}
+
+void Send_Battery_Voltage(float unused){
+    char command;
+    float runPeriod = task_send_battery_voltage.run_period;
+    if(runPeriod<0){ // detects whether the command was 'b' or 'B'
+        command = 'b';
+    }else{
+        command = 'B';
+    }
+    float voltage; // value to store voltage reading
+    voltage = Filter_Value(&voltage_filter, Battery_Voltage()); // filter voltage reading value
+    USB_Send_Msg("cf", command, &voltage, sizeof(voltage)); // send message with the battery voltage
+}
+
+void Send_Battery_Warning(float unused){
+    float voltage = Filter_Value(&voltage_filter, Battery_Voltage());
+    if (voltage > LOW_BATTERY_THRESHOLD_SW_OFF && voltage < LOW_BATTERY_THRESHOLD_SW_ON){
+        struct __attribute__((__packed__)) {
+            char let[7];
+            float volt;
+        }msg = {.let = {'B','A','T',' ','L','O','W'}, .volt = voltage};
+        // Send Warning to Serial that batteries need to be charged
+        USB_Send_Msg("c7sf",'!', &msg, sizeof(msg));
+    }
+}
