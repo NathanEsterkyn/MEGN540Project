b709950a36a88e8bce0285a7c83e3c1a04cf78d7
Nathan Esterkyn <nesterkyn@mines.edu>
2024-03-12 16:52:50 -0600

    Testing

--- c_lib/Lab4_Tasks.c
+++ c_lib/Lab4_Tasks.c
@@ -17,21 +17,21 @@ void Set_PWM_Value(int16_t Left, int16_t Right){
 
 bool Battery_Check(float unused){
     float voltage = Filter_Value(&voltage_filter, Battery_Voltage());
-    if ( voltage < LOW_BATTERY_THRESHOLD_SW_ON){
+    if (voltage < 1.0){
+        struct __attribute__((__packed__)) {
+            char let[9];
+        }msg = {.let = {'P','O','W','E','R',' ','O','F','F'}};
+        // Send Warning to Serial that batteries need to be charged
+        USB_Send_Msg("c9s",'!', &msg, sizeof(msg));
+        return false;
+    }
+    if (voltage < 3.6){
         struct __attribute__((__packed__)) {
             char let[7];
             float volt;
         }msg = {.let = {'B','A','T',' ','L','O','W'}, .volt = voltage};
         // Send Warning to Serial that batteries need to be charged
         USB_Send_Msg("c7sf",'!', &msg, sizeof(msg));
-        return false;
-    }
-    if ((voltage > LOW_BATTERY_THRESHOLD_SW_OFF){
-        struct __attribute__((__packed__)) {
-            char let[9];
-        }msg = {.let = {'P','O','W','E','R',' ','O','F','F'}};
-        // Send Warning to Serial that batteries need to be charged
-        USB_Send_Msg("c9s",'!', &msg, sizeof(msg));
         return false;
     }
     else {
