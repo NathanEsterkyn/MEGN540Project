0bb1cb92ad5255ed50852a7a1dc2618d2e659fba
Nathan Esterkyn <nesterkyn@mines.edu>
2024-03-16 14:59:12 -0600

    test

--- c_lib/Encoder.c
+++ c_lib/Encoder.c
@@ -189,7 +189,7 @@ ISR( INT6_vect )
         _last_right_A = !_last_right_A;
     }
     **/
-    _right_counts += ( Right_A() ^ _last_left_B ) - ( _last_right_A ^ Right_B() );
+    _right_counts += ( Right_A() ^ _last_right_B ) - ( _last_right_A ^ Right_B() );
     _last_right_B = Right_B();
     _last_right_A = Right_A();
 }
