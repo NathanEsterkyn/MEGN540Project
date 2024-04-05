3527dad5ccbfab47d1c2a16a9b723070d9208dfd
Nathan Esterkyn <nesterkyn@mines.edu>
2024-04-05 16:30:10 -0600

    Testing

--- c_lib/Lab5_Tasks.c
+++ c_lib/Lab5_Tasks.c
@@ -1,7 +1,9 @@
 #include "Lab5_Tasks.h"
 
 void Send_Distance(float unused) {
-
+    // enable PWM
+    // get a controller value (update)
+    // send the value to the motors
 }
 
 void Send_Velocity(float unused) {
