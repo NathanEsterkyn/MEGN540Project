47b1b00171586e752d3660c9acba1b3c8ee0a62a
Nathan Esterkyn <nesterkyn@mines.edu>
2024-04-06 18:56:57 -0600

    Testing

--- c_lib/Lab5_Tasks.c
+++ c_lib/Lab5_Tasks.c
@@ -17,25 +17,25 @@ void Send_Distance(float unused) {
     // FOR TESTING PURPOSES:
 
     struct __attribute__( ( __packed__ ) ) {
-        //float distance;
+        float distance;
         //float error;
         float PWM;
     } data_L;
-    //data_L.distance = left_measurement;
+    data_L.distance = left_measurement;
     //data_L.error = Left_Controller.target_pos - left_measurement;
     data_L.PWM = new_left;
 
     struct __attribute__( ( __packed__ ) ) {
-        //float distance;
+        float distance;
         //float error;
         float PWM;
     } data_R;
-    //data_R.distance = right_measurement;
+    data_R.distance = right_measurement;
     //data_R.error = Right_Controller.target_pos - right_measurement;
     data_R.PWM = new_right;
 
-    USB_Send_Msg("cf", 'L',  &data_L, sizeof( data_L ) );
-    USB_Send_Msg("cf", 'R',  &data_R, sizeof( data_R ) );
+    USB_Send_Msg("c2f", 'L',  &data_L, sizeof( data_L ) );
+    USB_Send_Msg("c2f", 'R',  &data_R, sizeof( data_R ) );
 
     MotorPWM_Enable( true ); // enable motors
 }
