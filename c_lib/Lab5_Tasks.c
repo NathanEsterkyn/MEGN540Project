053c9bf22867fc225146c15c3f0e2eda8e546e40
Nathan Esterkyn <nesterkyn@mines.edu>
2024-04-06 18:34:24 -0600

    Testing

--- c_lib/Lab5_Tasks.c
+++ c_lib/Lab5_Tasks.c
@@ -17,21 +17,21 @@ void Send_Distance(float unused) {
     // FOR TESTING PURPOSES:
 
     struct __attribute__( ( __packed__ ) ) {
-        float distance;
-        //float error;
+        //float distance;
+        float error;
         //float PWM;
     } data_L;
-    data_L.distance = left_measurement;
-    //data_L.error = Left_Controller.target_pos - left_measurement;
+    //data_L.distance = left_measurement;
+    data_L.error = Left_Controller.target_pos - left_measurement;
     //data_L.PWM = new_left;
 
     struct __attribute__( ( __packed__ ) ) {
-        float distance;
-        //float error;
+        //float distance;
+        float error;
         //float PWM;
     } data_R;
-    data_R.distance = right_measurement;
-    //data_R.error = Right_Controller.target_pos - right_measurement;
+    //data_R.distance = right_measurement;
+    data_R.error = Right_Controller.target_pos - right_measurement;
     //data_R.PWM = new_right;
 
     USB_Send_Msg("cf", 'L',  &data_L, sizeof( data_L ) );
