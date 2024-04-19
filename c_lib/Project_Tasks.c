c7ff548230bd10a660dc68bb6e568b9a8865e6ea
Nathan Esterkyn <nesterkyn@mines.edu>
2024-04-19 01:07:23 -0600

    Testing

--- c_lib/Project_Tasks.c
+++ c_lib/Project_Tasks.c
@@ -1,1 +1,13 @@
 #include "Project_Tasks.h"
+
+void Home(float unused) {
+    // homing function
+}
+
+void Erase(float unused) {
+    // spiral function
+}
+
+void Disable_Motors(float unused) {
+    // activate standby function on motor driver
+}
