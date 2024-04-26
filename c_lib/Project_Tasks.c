db25bf25ca46ea9139f1fd09e2accb5633d53d0a
yameatmeyourdead <zac71113@gmail.com>
2024-04-26 11:49:19 -0600

    clang is a bitch

--- c_lib/Project_Tasks.c
+++ c_lib/Project_Tasks.c
@@ -1,100 +1,119 @@
 #include "Project_Tasks.h"
 
-void Home(float unused) {
+void Home( float unused )
+{
     // Linear Homing
     Sandworm_Robot.Lin_vel = HOME_SPEED_L;
-    Stepper_Speed( &Sandworm_Robot.Linear, HOME_SPEED_L ); // set homing speed
-    Task_Activate( &task_enable_motors, -1 ); // move linear axis towards home
-    if( Sandworm_Robot.limitState == 1 ) { // if home button is pressed
-        Task_Activate( &task_stop_step, -1 ); // stop the motors
-        Sandworm_Robot.Lin_pos = 0.0; // set zero
+    Stepper_Speed( &Sandworm_Robot.Linear, HOME_SPEED_L );  // set homing speed
+    Task_Activate( &task_enable_motors, -1 );               // move linear axis towards home
+    if( Sandworm_Robot.limitState == 1 ) {                  // if home button is pressed
+        Task_Activate( &task_stop_step, -1 );               // stop the motors
+        Sandworm_Robot.Lin_pos = 0.0;                       // set zero
     }
 }
 
-void Erase(float unused) {
+void Erase( float unused )
+{
 
-    Task_Activate( &task_home, -1 ); // spiral function: home, travel to outside, spiral inwards to 0,0
+    Task_Activate( &task_home, -1 );  // spiral function: home, travel to outside, spiral inwards to 0,0
     Sandworm_Robot.Lin_vel = ERASE_SPEED_L;
     Sandworm_Robot.Rot_vel = ERASE_SPEED_R;
     Stepper_Speed( &Sandworm_Robot.Linear, ERASE_SPEED_L );
-    Stepper_Speed( &Sandworm_Robot.Rotary, ERASE_SPEED_R ); // set spiral speeds
+    Stepper_Speed( &Sandworm_Robot.Rotary, ERASE_SPEED_R );  // set spiral speeds
     Task_Activate( &task_enable_motors, -1 );
-    if ( Sandworm_Robot.Lin_pos >= Sandworm_Robot.Radius ) { // if the position of the linear actuator has reached the radius
-        Task_Activate( &task_stop_step, -1 ); // stop the motors
+    if( Sandworm_Robot.Lin_pos >= Sandworm_Robot.Radius ) {  // if the position of the linear actuator has reached the radius
+        Task_Activate( &task_stop_step, -1 );                // stop the motors
     }
 }
 
-void Enable_Motors(float unused) {
-    if ( Sandworm_Robot.Lin_vel != 0 ) {
-        Stepper_Enable( &Sandworm_Robot.Linear ); // enables motors if their speed is not 0
+void Enable_Motors( float unused )
+{
+    if( Sandworm_Robot.Lin_vel != 0 ) {
+        Stepper_Enable( &Sandworm_Robot.Linear );  // enables motors if their speed is not 0
     }
-    if ( Sandworm_Robot.Rot_vel != 0 ) {
+    if( Sandworm_Robot.Rot_vel != 0 ) {
         Stepper_Enable( &Sandworm_Robot.Rotary );
     }
+    float linval = 0.0;
+    USB_Send_Msg( "cff", 'E', &linval, sizeof( linval ) );  // print out the command and the time
 }
 
-void Disable_Motors(float unused) {
+void Disable_Motors( float unused )
+{
     float linval = 2.0;
-    USB_Send_Msg("cff", 'D' , &linval , sizeof(linval)); // print out the command and the time
-    Stepper_Disable( &Sandworm_Robot.Linear ); // disables both motors
+    USB_Send_Msg( "cff", 'D', &linval, sizeof( linval ) );  // print out the command and the time
+    Stepper_Disable( &Sandworm_Robot.Linear );              // disables both motors
     Stepper_Disable( &Sandworm_Robot.Rotary );
 }
 
-void Stop_Step(float unused) {
+void Stop_Step( float unused )
+{
     Task_Cancel( &task_enable_motors );
-    Task_Activate( &task_disable_motors, -1 ); // disables motors
-    Sandworm_Robot.Lin_vel = 0.0; // set the velocity to zero
+    Task_Activate( &task_disable_motors, -1 );  // disables motors
+    Sandworm_Robot.Lin_vel = 0.0;               // set the velocity to zero
     Sandworm_Robot.Rot_vel = 0.0;
-    float linval = 1.0;
-    USB_Send_Msg("cff", 'S' , &linval , sizeof(linval)); // print out the command and the time
-    Task_Cancel( &task_stop_step ); // cancels itself
+    float linval           = 1.0;
+    USB_Send_Msg( "cff", 'S', &linval, sizeof( linval ) );  // print out the command and the time
+    Task_Cancel( &task_stop_step );                         // cancels itself
 }
 
-void Button_Check(float unused) {
+void Button_Check( float unused )
+{
 
-    if ( Sandworm_Robot.buttonState == 1 ) { // checks if the power button is on
-        PORTD |= ( 1 << PORTD1 ); // turns on the power LED
-    }
-    else {
+    if( Sandworm_Robot.buttonState == 1 ) {  // checks if the power button is on
+        PORTD |= ( 1 << PORTD1 );            // turns on the power LED
+    } else {
         PORTD &= ~( 1 << PORTD1 );
     }
 }
 
-void Send_Switch_Status(float _time_since_last) { Send_Switch_Message('W'); }
+void Send_Switch_Status( float _time_since_last )
+{
+    Send_Switch_Message( 'W' );
+}
 
-void Send_Switch_Message(char message_type) {
-  struct __attribute__((__packed__)) {
-    bool status;
-  } data;
+void Send_Switch_Message( char message_type )
+{
+    struct __attribute__( ( __packed__ ) ) {
+        bool status;
+    } data;
 
-  data.status = Sandworm_Robot.limitState;
+    data.status = Sandworm_Robot.limitState;
 
-  USB_Send_Msg("cb", message_type, &data, sizeof(data));
+    USB_Send_Msg( "cb", message_type, &data, sizeof( data ) );
 }
 
 // Timing ISRs:
 
-ISR( TIMER1_COMPA_vect ) // performs steps for motor 1 (rotary on PORTB)
+ISR( TIMER1_COMPA_vect )  // performs steps for motor 1 (rotary on PORTB)
 {
-    Stepper_Step( &Sandworm_Robot.Rotary ); // steps rotary motor once
-    if ( Sandworm_Robot.Rotary.direction == 1 ) { Sandworm_Robot.Rot_pos ++; } // increment position depending on direction
-    if ( Sandworm_Robot.Rotary.direction == 0 ) { Sandworm_Robot.Rot_pos --; }// mode is set to clear counter on compare match so no need to reset counter value here
+    Stepper_Step( &Sandworm_Robot.Rotary );  // steps rotary motor once
+    if( Sandworm_Robot.Rotary.direction == 1 ) {
+        Sandworm_Robot.Rot_pos++;
+    }  // increment position depending on direction
+    if( Sandworm_Robot.Rotary.direction == 0 ) {
+        Sandworm_Robot.Rot_pos--;
+    }  // mode is set to clear counter on compare match so no need to reset counter value here
 }
 
-ISR( TIMER3_COMPA_vect ) // performs steps for motor 2 (Linear on PORTF)
+ISR( TIMER3_COMPA_vect )  // performs steps for motor 2 (Linear on PORTF)
 {
-    Stepper_Step( &Sandworm_Robot.Linear ); // steps linear motor once
-    if ( Sandworm_Robot.Linear.direction == 1 ) { Sandworm_Robot.Lin_pos ++; } // increment position depending on direction
-    if ( Sandworm_Robot.Linear.direction == 0 ) { Sandworm_Robot.Lin_pos --; }
+    Stepper_Step( &Sandworm_Robot.Linear );  // steps linear motor once
+    if( Sandworm_Robot.Linear.direction == 1 ) {
+        Sandworm_Robot.Lin_pos++;
+    }  // increment position depending on direction
+    if( Sandworm_Robot.Linear.direction == 0 ) {
+        Sandworm_Robot.Lin_pos--;
+    }
 }
-ISR( INT0_vect ) // ISR for handling a limit switch press
+ISR( INT0_vect )  // ISR for handling a limit switch press
 {
-    Sandworm_Robot.limitState = PIND & (1 << PIND0);
+    Sandworm_Robot.limitState = PIND & ( 1 << PIND0 );
     // ADD A METHOD FOR DEBOUNCING!!
 }
 
-ISR( INT2_vect ) // ISR for handling a power button press
+ISR( INT2_vect )  // ISR for handling a power button press
 {
-    //Sandworm_Robot.buttonState = !Sandworm_Robot.buttonState;
-    // ADD A METHOD FOR DEBOUNCING!!
+    // Sandworm_Robot.buttonState = !Sandworm_Robot.buttonState;
+    //  ADD A METHOD FOR DEBOUNCING!!
 }
--- c_lib/Stepper.c
+++ c_lib/Stepper.c
@@ -25,131 +25,139 @@
  *   MP4   -  BIN2  - Pin 39 - PF4(ADC4)
  */
 
-void Initialize_Stepper( Stepper_t* p_step, int number_of_steps, int motor_pin_1 ) {
+void Initialize_Stepper( Stepper_t* p_step, int number_of_steps, int motor_pin_1 )
+{
 
     // Fill stepper structure with relevant data
     p_step->number_of_steps = number_of_steps;
-    p_step->motor_pin_1 = motor_pin_1;
-    p_step->last_step_time = 0.0;
-    p_step->direction = 1;
-    p_step->step_number = 0;
-    p_step->step_delay = 0.0;
+    p_step->motor_pin_1     = motor_pin_1;
+    p_step->last_step_time  = 0.0;
+    p_step->direction       = 1;
+    p_step->step_number     = 0;
+    p_step->step_delay      = 0.0;
 
     // Set DDR for both motors
-    if ( p_step->motor_pin_1 == 8 ) { DDRB |= 0xF; } // sets PB 0,1,2,and 3 to outputs
-    if ( p_step->motor_pin_1 == 36 ) { DDRF |= 0xF0; } // sets PF 4,5,6,and 7 to outputs
+    if( p_step->motor_pin_1 == 8 ) {
+        DDRB |= 0xF;
+    }  // sets PB 0,1,2,and 3 to outputs
+    if( p_step->motor_pin_1 == 36 ) {
+        DDRF |= 0xF0;
+    }  // sets PF 4,5,6,and 7 to outputs
 
     // Interrupts on Timer/Counter3 will trigger each step, setup timer1 and timer3:
-    TCCR1A = 0x00; // sets bits to zero - Normal port operation, OC1A and OC1B disconnected (no output to ports)
-    TCCR1B |= ( 1 << WGM12 ); // select mode 4 - clear timer on compare match
-    TCCR1B |= ( 1 << CS11 ); // select a prescalar of 8 - 2000 pulses per millisecond
-    TIMSK1 &= ~( 1 << OCIE1A ); // disables output compare match with the OCR1A register
+    TCCR1A = 0x00;               // sets bits to zero - Normal port operation, OC1A and OC1B disconnected (no output to ports)
+    TCCR1B |= ( 1 << WGM12 );    // select mode 4 - clear timer on compare match
+    TCCR1B |= ( 1 << CS11 );     // select a prescalar of 8 - 2000 pulses per millisecond
+    TIMSK1 &= ~( 1 << OCIE1A );  // disables output compare match with the OCR1A register
 
-    TCCR3A = 0x00; // sets bits to zero - Normal port operation, OC1A and OC1B disconnected (no output to ports)
-    TCCR3B |= ( 1 << WGM32 ); // select mode 4 - clear timer on compare match
-    TCCR3B |= ( 1 << CS31 ); // select a prescalar of 8 - 2000 pulses per millisecond
-    TIMSK3 &= ~( 1 << OCIE3A ); // disables output compare match with the OCR3A register
+    TCCR3A = 0x00;               // sets bits to zero - Normal port operation, OC1A and OC1B disconnected (no output to ports)
+    TCCR3B |= ( 1 << WGM32 );    // select mode 4 - clear timer on compare match
+    TCCR3B |= ( 1 << CS31 );     // select a prescalar of 8 - 2000 pulses per millisecond
+    TIMSK3 &= ~( 1 << OCIE3A );  // disables output compare match with the OCR3A register
 
-    OCR1A = 0; // initialize compare value: 2000 means the clock will clear and the ISR will run each millisecond
-    OCR3A = 0; // initialize compare value: 2000 means the clock will clear and the ISR will run each millisecond
+    OCR1A = 0;  // initialize compare value: 2000 means the clock will clear and the ISR will run each millisecond
+    OCR3A = 0;  // initialize compare value: 2000 means the clock will clear and the ISR will run each millisecond
 }
 
-void Stepper_Speed( Stepper_t* p_step, float Value) {
+void Stepper_Speed( Stepper_t* p_step, float Value )
+{
 
-    if ( Value > 0.0 ) { // if the motor is moving positive direction
+    if( Value > 0.0 ) {  // if the motor is moving positive direction
         p_step->direction = 1;
     }
-    if ( Value < 0.0 ) {
+    if( Value < 0.0 ) {
         p_step->direction = 0;
-        Value = -Value;
+        Value             = -Value;
     }
 
-    float steps = p_step->number_of_steps; // converts number of steps per revolution into float
-    steps = ( Value / 60.0 ) * steps;
-    p_step->step_delay =  ( 1.0 / steps ) * 1000.0; // returns milliseconds between steps to achieve desired speed in RPM
+    float steps        = p_step->number_of_steps;  // converts number of steps per revolution into float
+    steps              = ( Value / 60.0 ) * steps;
+    p_step->step_delay = ( 1.0 / steps ) * 1000.0;  // returns milliseconds between steps to achieve desired speed in RPM
 
-    uint32_t delay_time = 2000 * p_step->step_delay; // converts to a 16 bit int to set registers
+    uint32_t delay_time = 2000 * p_step->step_delay;  // converts to a 16 bit int to set registers
 
-    if ( p_step->motor_pin_1 == 8 ) { // motor 1 is on timer1
+    if( p_step->motor_pin_1 == 8 ) {  // motor 1 is on timer1
         OCR1A = delay_time;
     }
-    if ( p_step->motor_pin_1 == 36 ) { // motor 2 is on timer3
+    if( p_step->motor_pin_1 == 36 ) {  // motor 2 is on timer3
         OCR3A = delay_time;
     }
 }
 
-void Stepper_Step( Stepper_t* p_step ) {
+void Stepper_Step( Stepper_t* p_step )
+{
 
     // Determine stepper direction
-    if ( p_step->direction == 1 ) {
+    if( p_step->direction == 1 ) {
         p_step->step_number++;
-        if ( p_step->step_number == p_step->number_of_steps) {
-            p_step->step_number = 0; // for rolling over
+        if( p_step->step_number == p_step->number_of_steps ) {
+            p_step->step_number = 0;  // for rolling over
         }
-    }
-    else {
-        if ( p_step->step_number == 0 ) {
+    } else {
+        if( p_step->step_number == 0 ) {
             p_step->step_number = p_step->number_of_steps;
         }
         p_step->step_number--;
     }
 
     // Step motor
-    if (p_step->motor_pin_1 == 8) { // if the selected stepper is Motor 1
-        switch (p_step->step_number % 4) {
-            case 0:  // 1010
-                PORTB = 0xA0; // sets PB 0,1,2,and 3
+    if( p_step->motor_pin_1 == 8 ) {  // if the selected stepper is Motor 1
+        switch( p_step->step_number % 4 ) {
+            case 0:            // 1010
+                PORTB = 0xA0;  // sets PB 0,1,2,and 3
                 break;
-            case 1:  // 0110
-                PORTB = 0x60; // sets PB 0,1,2,and 3
+            case 1:            // 0110
+                PORTB = 0x60;  // sets PB 0,1,2,and 3
                 break;
-            case 2:  //0101
-                PORTB = 0x50; // sets PB 0,1,2,and 3
+            case 2:            // 0101
+                PORTB = 0x50;  // sets PB 0,1,2,and 3
                 break;
-            case 3:  //1001
-                PORTB = 0x90; // sets PB 0,1,2,and 3
+            case 3:            // 1001
+                PORTB = 0x90;  // sets PB 0,1,2,and 3
                 break;
         }
     }
-    if (p_step->motor_pin_1 == 36) { // if the selected stepper is Motor 2
-        float ret_val = p_step->step_number; // FOR TESTING
-        USB_Send_Msg( "cf", 'L', &ret_val, sizeof( ret_val ) ); // FOR TESTING
+    if( p_step->motor_pin_1 == 36 ) {                            // if the selected stepper is Motor 2
+        float ret_val = p_step->step_number;                     // FOR TESTING
+        USB_Send_Msg( "cf", 'L', &ret_val, sizeof( ret_val ) );  // FOR TESTING
 
-        switch (p_step->step_number % 4) {
-            case 0:  // 1010
-                PORTF = 0xA0; // sets PF5 and PF7 to HIGH and all others to LOW
+        switch( p_step->step_number % 4 ) {
+            case 0:            // 1010
+                PORTF = 0xA0;  // sets PF5 and PF7 to HIGH and all others to LOW
                 break;
-            case 1:  // 0110
-                PORTF = 0x60; // sets PF5 and PF6 to HIGH and all others to LOW
+            case 1:            // 0110
+                PORTF = 0x60;  // sets PF5 and PF6 to HIGH and all others to LOW
                 break;
-            case 2:  //0101
-                PORTF = 0x50; // sets PF4 and PF6 to HIGH and all others to LOW
+            case 2:            // 0101
+                PORTF = 0x50;  // sets PF4 and PF6 to HIGH and all others to LOW
                 break;
-            case 3:  //1001
-                PORTF = 0x90; // sets PF4 and PF7 to HIGH and all others to LOW
+            case 3:            // 1001
+                PORTF = 0x90;  // sets PF4 and PF7 to HIGH and all others to LOW
                 break;
         }
     }
 }
 
-void Stepper_Disable( Stepper_t* p_step ) {
-    if (p_step->motor_pin_1 == 8) { // if the selected stepper is Motor 1
-        TIMSK1 &= ~( 1 << OCIE1A ); // disables output compare match with the OCR1A register
+void Stepper_Disable( Stepper_t* p_step )
+{
+    if( p_step->motor_pin_1 == 8 ) {  // if the selected stepper is Motor 1
+        TIMSK1 &= ~( 1 << OCIE1A );   // disables output compare match with the OCR1A register
         PORTB = 0x00;
     }
-    if (p_step->motor_pin_1 == 36) { // if the selected stepper is Motor 2
-        TIMSK3 &= ~( 1 << OCIE3A ); // disables output compare match with the OCR1A register
+    if( p_step->motor_pin_1 == 36 ) {  // if the selected stepper is Motor 2
+        TIMSK3 &= ~( 1 << OCIE3A );    // disables output compare match with the OCR1A register
         PORTF = 0x00;
     }
 }
 
-void Stepper_Enable( Stepper_t* p_step ) {
+void Stepper_Enable( Stepper_t* p_step )
+{
     cli();
-    if (p_step->motor_pin_1 == 8) { // if the selected stepper is Motor 1
-        TIMSK1 |= ( 1 << OCIE1A ); // disables output compare match with the OCR1A register
+    if( p_step->motor_pin_1 == 8 ) {  // if the selected stepper is Motor 1
+        TIMSK1 |= ( 1 << OCIE1A );    // disables output compare match with the OCR1A register
     }
-    if (p_step->motor_pin_1 == 36) { // if the selected stepper is Motor 2
-        TIMSK3 |= ( 1 << OCIE3A ); // disables output compare match with the OCR1A register
+    if( p_step->motor_pin_1 == 36 ) {  // if the selected stepper is Motor 2
+        TIMSK3 |= ( 1 << OCIE3A );     // disables output compare match with the OCR1A register
     }
     sei();
 }
